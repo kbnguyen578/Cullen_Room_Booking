b092187a45ced067a65feaec77cdf0022ad92b6a
Kim Nguyen <kongimong@Kims-MacBook-Air-4.local>
2026-06-17 00:43:34 -0500

    laying out the sheet structure to plan on how to get data

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
+# accessing the google sheets API
 import gspread
 
+# opening up the google sheets 
 gc = gspread.service_account()
+SPREADSHEET_NAME = "test_cullen"
+sh = gc.open(SPREADSHEET_NAME)
 
-sh = gc.open("test_cullen")
-
+# testing -- it works! 
 print(sh.sheet1.get('C2'))
+
+# columns + their field 
+title_col = 'C'
+date_col = 'D'
+desc_col = 'E'
+start_time_col = 'F'
+end_time_col = 'G'
+loc_1_col = 'P'
+loc_2_col = 'Q'
+loc_3_col = 'R'
+building_col = 'H'
+
