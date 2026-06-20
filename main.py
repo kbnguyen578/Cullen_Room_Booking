51160d3c003299e54aa93061949b238df73b2a8c
Kim Nguyen <kongimong@Kims-MacBook-Air-4.local>
2026-06-20 04:58:14 -0500

    retrieved data needed to book room exclusively for cullen building

--- main.py
+++ main.py
@@ -4,19 +4,39 @@ import gspread
 # opening up the google sheets 
 gc = gspread.service_account()
 SPREADSHEET_NAME = "test_cullen"
-sh = gc.open(SPREADSHEET_NAME)
+sh = gc.open(SPREADSHEET_NAME)  # this is the ENTIRE wksh 
+worksheet = sh.sheet1           # this is the specific tab we work with 
 
 # testing -- it works! 
 print(sh.sheet1.get('C2'))
 
 # columns + their field 
-title_col = 'C'
-date_col = 'D'
-desc_col = 'E'
-start_time_col = 'F'
-end_time_col = 'G'
-loc_1_col = 'P'
-loc_2_col = 'Q'
-loc_3_col = 'R'
-building_col = 'H'
+# title_col = 'C'       | [2] 
+# date_col = 'D'        | [3]
+# desc_col = 'E'        | [4] 
+# start_time_col = 'F'  | [5]
+# end_time_col = 'G'    | [6] 
+# loc_1_col = 'P'       | [15]
+# loc_2_col = 'Q'       | [16]
+# loc_3_col = 'R'       | [17]
+# building_col = 'H'    | [7] (0-based)
 
+all_rows = worksheet.get_all_values() 
+TARGET_BUILDING = "Cullen College of Engineering Building"
+event_data = []
+
+#  retrieve data for only Cullen College forms, ignore the header row 
+for row in all_rows[1:]: 
+    if row[7] == TARGET_BUILDING: 
+        event_data.append({
+            "title":        row[2], 
+            "date":         row[3], 
+            "desc":         row[4],
+            "start_time":   row[5], 
+            "end_time":     row[6], 
+            "loc_1":        row[15], 
+            "loc_2":        row[16], 
+            "loc_3":        row[17]
+        })
+
+print(event_data[0])
