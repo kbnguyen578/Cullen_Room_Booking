4ea5ce3d571e66eff0315661fff72e7d0af8019e
Kim Nguyen <kongimong@Kims-MacBook-Air-4.local>
2026-06-21 16:28:55 -0500

    tested and successfully filled out the contact infomration field of website

--- autofill.py
+++ autofill.py
@@ -8,11 +8,69 @@ from selenium.webdriver.support.ui import Select, WebDriverWait
 # ExpCond + WebDriverWait => until element visible/clickable 
 from selenium.webdriver.support import expected_conditions as ExpCond
 
-# for CHROME 
+# ==== for CHROME -- uncomment the section below & comment out Safari ====
 # from webdriver_manager.chrome import ChromeDriverManager 
 # from selenium.webdriver.chrome.service import Service 
 
-# for SAFARI
-# Safari > Settings > Advanced > "show features for web developers" 
-# Safari > Settings > Developer > "Allow Remote Automation "
-driver = webdriver.Safari  
+URL = "https://www.egr.uh.edu/forms/room-reservation-form"
+ORGANIZATION = "SASE"
+
+def fill_form(event_data, config): 
+    # ==== for CHROME ==== 
+    # driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+    # ==== for SAFARI -- follow instructions & uncomment line 18 & comment out Chrome ====
+    # Safari > Settings > Advanced > "show features for web developers" 
+    # Safari > Settings > Developer > "Allow Remote Automation "
+    driver = webdriver.Safari()  
+
+    driver.get(URL)
+    wait = WebDriverWait(driver, 10)
+
+    # field: Contact Information 
+    wait.until(ExpCond.presence_of_element_located((By.NAME, "first_name")))
+    # first name | name="first_name"
+    driver.find_element(By.NAME, "first_name").send_keys(config["ec_first"])
+
+    # last name | name="last_name"
+    driver.find_element(By.NAME, "last_name").send_keys(config["ec_last"])
+
+    # email | name="email_address"
+    driver.find_element(By.NAME, "email_address").send_keys(config["ec_email"])
+
+    # PSID | name="peoplesoft_id"
+    driver.find_element(By.NAME, "peoplesoft_id").send_keys(config["ec_psid"])
+
+    # Organization | name="department"
+    driver.find_element(By.NAME, "department").send_keys(ORGANIZATION)
+
+    # Phone Number | name="phone"
+    driver.find_element(By.NAME, "phone").send_keys(config["ec_phone_number"])
+
+    # field: Room is requested for 
+    # first name | name="requested_for_first_name"
+
+    # last name | name="requested_for_last_name"
+
+    # field: Reservation Information 
+    # event date | name="event_date"
+
+    # start time | name="start_time"
+
+    # end time | name="end_time"
+
+    # event title | name="event_title"
+
+    # number fo attendees | name="number_of_attendees"
+
+    # locattion 1st choice | name="location1"
+
+    # location 2nd choice | name="location2"
+
+    # location 3rd choice | name="location3"
+
+    # event description | name="description"
+
+    input("Press Enter here once you have submitted the form...")
+
+    driver.quit()
--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import json 
 from sheets import get_cullen_events
+from autofill import fill_form
 
 # loading in the Event Coordinator's info 
 with open("config.json") as f: 
     config = json.load(f)
 
+# calling sheets.py to get the Cullen Event Data from google sheets 
 event_data = get_cullen_events()
 
 # testing to see if sheets.py function works -- it does! 
 # print(event_data[0])
+
+# testing fill form 
+fill_form(event_data[0], config)
